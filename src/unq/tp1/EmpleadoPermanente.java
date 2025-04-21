60b1e5ead86689331d0d4bc968a9897bea064910
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-04-21 01:30:06 -0300

    Definición de generarReciboDeHaberes del empleado permanente

--- src/unq/tp1/EmpleadoPermanente.java
+++ src/unq/tp1/EmpleadoPermanente.java
@@ -32,6 +32,13 @@ public class EmpleadoPermanente extends Empleado{
     }
 
     public double sueldoBruto() {
+        /*
+         * Sueldo Básico
+         * Salario Familiar, que se compone de:
+         * * Asignación por hijo: $150 por cada hijo.
+         * * Asignación por cónyuge: $100 si tiene cónyuge
+         * Antigüedad: $50 por cada año de antigüedad
+         */
         int asignacionConyugal = (this.getEstadoCivil() == "Casado") ? 100 : 0;
         int asignacionPorHijo = 150 * this.cantHijos;
         int antiguedad = 50 * this.aniosDeAntiguedad;
@@ -53,4 +60,25 @@ public class EmpleadoPermanente extends Empleado{
         return obraSocial + aporteJubilatorio;
     }
 
+    public String generarReciboDeHaberes() {
+        double sueldoBruto = this.sueldoBruto();
+
+        String recibo = 
+            "Nombre: " + this.nombre + "\n" + 
+            "Direccion: " + this.direccion + "\n" + 
+            "Fecha de emision: " + LocalDate.now().toString()+ "\n" +
+            "Sueldo bruto: " + sueldoBruto + "\n" +
+            "Sueldo neto: " + this.sueldoNeto() + "\n" +
+            "Desgloce de conceptos: " + "\n" +
+            "Sueldo bruto = " + sueldoBruto + "\n" +
+            "* Sueldo basico: " + this.sueldoBasico + "\n" +
+            "* Antiguedad: $50 por anio (" + this.aniosDeAntiguedad + ") = $" + this.aniosDeAntiguedad*50 + "\n" +
+            "* Salario familiar: $150 por hijo ($" + this.cantHijos*150 + ") + $100 si tiene conyuge ($" + ((this.getEstadoCivil() == "Casado") ? 100 : 0) + ")" + "\n" +
+            "Retenciones = " + this.retenciones() + "\n" +
+            "* Obra social: 10% del sueldo bruto ($" + sueldoBruto*0.1 + ") + $20 por hijo ($" + this.cantHijos*20 + ") = $" + (sueldoBruto*0.1 +this.cantHijos*20) + "\n" +
+            "* Aporte jubilatorio: 15% del sueldo bruto ($" + sueldoBruto*0.15 + ")\n" +
+            " ------------------ " + "\n";
+        
+        return recibo;
+    }
 }
