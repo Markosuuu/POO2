57838f68d8c24d6cb9eb1738aee597bd1bbfdf45
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-04-27 16:27:02 -0300

    update de Supermercado

--- src/unq/tp4/Supermercado.java
+++ src/unq/tp4/Supermercado.java
@@ -1,58 +1,57 @@
 package unq.tp4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Supermercado {
     private String nombre;
     private String direccion;
-    private Producto[] productos;
+    // private Producto[] productos = {};
+    private List<Producto> productos = new ArrayList<Producto>();
     
     
-    public Supermercado(String nombre, String direccion, Producto[] productos) {
+    public Supermercado(String nombre, String direccion, List<Producto> productos) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.productos = productos;
     }
-
+    
+    public Supermercado(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
 
     public String getNombre() {
         return nombre;
     }
 
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
 
     public String getDireccion() {
         return direccion;
     }
 
-
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-
-    public Producto[] getProductos() {
-        return productos;
-    }
-
-
-    public void setProductos(Producto[] productos) {
-        this.productos = productos;
-    }
-
     public int cantProductos() {
-        return productos.length;
+        return this.productos.size();
     }
 
-    public double sumatorioPreciosProductos() {
+    public double sumatorioPrecios() {
         double suma = 0;
-
-        for (Producto producto : productos) {
-            suma += producto.precioFinal();
+        
+        for (int i = 0; i < productos.size(); i++) {
+            suma += productos.get(i).precioFinal();
         }
 
         return suma;
+    }
+
+    public void agregarProducto(Producto prod) {
+        this.productos.add(prod);
     }
 }
