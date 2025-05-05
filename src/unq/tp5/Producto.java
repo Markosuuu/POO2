877254480da68b39c8521519295bf04f682c240a
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-05-04 21:33:26 -0300

    Creación de la clase Producto

---
+++ src/unq/tp5/Producto.java
@@ -0,0 +1,27 @@
+package unq.tp5;
+
+public class Producto {
+    private String nombre;
+    private double precioBase;
+
+    public Producto(String nombre, double precioBase) {
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public double getPrecioFinal() {
+        return getPrecioBase();
+    }
+}
