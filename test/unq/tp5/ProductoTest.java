b2e53b79bc4119b6b85c187897b66d5aa84db24b
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-05-04 21:34:51 -0300

    Test de Producto

---
+++ test/unq/tp5/ProductoTest.java
@@ -0,0 +1,35 @@
+package unq.tp5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductoTest {
+
+    private Producto harina;
+
+    @BeforeEach
+    public void setUp() {
+        harina = new Producto("Harina", 17.5d);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Harina", harina.getNombre());
+        assertEquals(17.5d, harina.getPrecioBase());
+    }
+    
+    @Test
+    public void testSetPrecioBase() {
+        harina.setPrecioBase(20d);
+        assertEquals(20d, harina.getPrecioBase());
+    }
+    
+    @Test
+    public void testGetPrecioFinal() {
+        assertEquals(17.5d, harina.getPrecioFinal());
+
+    }
+
+}
